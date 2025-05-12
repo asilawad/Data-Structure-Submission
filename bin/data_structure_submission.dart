021fca414d2bcfaa718aacfab5679873ec496993
Asil Awad <aseelawad8@hotmail.com>
2025-05-13 02:12:32 +0300

    first commit

---
+++ bin/data_structure_submission.dart
@@ -0,0 +1,5 @@
+import 'package:data_structure_submission/data_structure_submission.dart' as data_structure_submission;
+
+void main(List<String> arguments) {
+  print('Hello world: ${data_structure_submission.calculate()}!');
+}
---
+++ test/data_structure_submission_test.dart
@@ -0,0 +1,8 @@
+import 'package:data_structure_submission/data_structure_submission.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
