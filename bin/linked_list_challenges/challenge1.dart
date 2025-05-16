3681ece325ea30848221e48cd0494158be711933
Asil Awad <aseelawad8@hotmail.com>
2025-05-16 22:14:34 +0300

    test middleNode function

--- bin/linked_list_challenges/challenge1.dart
+++ bin/linked_list_challenges/challenge1.dart
@@ -1,6 +1,13 @@
 import 'package:data_structure_submission/linked_list.dart';
 
+void printNodesInReverse<T>(Node<T>? node) {
+  if (node == null) return;
+
+  printNodesInReverse(node.next);
+
+  print(node.value);
+}
 
 void printListInReverse<E>(LinkedList<E> list) {
- printNodesInReverse(list.head);
+  printNodesInReverse(list.head);
 }
--- bin/linked_list_challenges/linked_list_challenges.dart
+++ bin/linked_list_challenges/linked_list_challenges.dart
@@ -1,10 +1,12 @@
 import 'package:data_structure_submission/linked_list.dart';
 
 import 'challenge1.dart';
+import 'challenge2.dart';
 
 void main(List<String> args) {
   //Linked List
   final list = LinkedList<int>();
+  list.push(4);
   list.push(3);
   list.push(2);
   list.push(1);
@@ -12,7 +14,13 @@ void main(List<String> args) {
   print(list);
 
 //print linked list in reverse
+  print("print linked list in reverse");
   print('Original list: $list');
   print("Printing in reverse:");
   printListInReverse(list);
+
+  // finde middle node
+  print("finde middle node");
+  final middleNode = getMiddle(list);
+  print('Middle: ${middleNode?.value}');
 }
