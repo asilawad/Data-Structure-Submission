564ca58635483f9f20b25cb9252c095c2c92d6bb
Asil Awad <aseelawad8@hotmail.com>
2025-05-16 21:08:52 +0300

    override toString

--- lib/stack.dart
+++ lib/stack.dart
@@ -1,4 +1,11 @@
 class Stack<E> {
   Stack() : _storage = <E>[];
   final List<E> _storage;
+
+  @override
+  String toString() {
+    return '--- Top ---\n'
+        '${_storage.reversed.join('\n')}'
+        '\n-----------';
+  }
 }
