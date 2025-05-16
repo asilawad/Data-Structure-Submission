3e0d3e6fbbfef9524ea8393070fb0b619d2cda0c
Asil Awad <aseelawad8@hotmail.com>
2025-05-16 22:02:01 +0300

    test linkedList class

---
+++ bin/linked_list_challenges/linked_list_challenges.dart
@@ -0,0 +1,9 @@
+import 'package:data_structure_submission/linked_list.dart';
+
+void main(List<String> args) {
+  final list = LinkedList<int>();
+  list.push(3);
+  list.push(2);
+  list.push(1);
+  print(list);
+}
