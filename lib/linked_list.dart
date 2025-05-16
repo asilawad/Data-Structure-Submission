cbcd8f1fe8910ca89184d5502ff591e46694f943
Asil Awad <aseelawad8@hotmail.com>
2025-05-16 21:54:58 +0300

    add toString for Node

--- lib/linked_list.dart
+++ lib/linked_list.dart
@@ -2,4 +2,10 @@ class Node<T> {
   Node({required this.value, this.next});
   T value;
   Node<T>? next;
+
+  @override
+  String toString() {
+    if (next == null) return '$value';
+    return '$value -> ${next.toString()}';
+  }
 }
