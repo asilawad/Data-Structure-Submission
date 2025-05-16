703e5299b9b087175f1a83d6f065860e2ec07a16
Asil Awad <aseelawad8@hotmail.com>
2025-05-16 22:08:35 +0300

    add printListInReverse functionality

---
+++ bin/linked_list_challenges/challenge1.dart
@@ -0,0 +1,6 @@
+import 'package:data_structure_submission/linked_list.dart';
+
+
+void printListInReverse<E>(LinkedList<E> list) {
+ printNodesInReverse(list.head);
+}
--- bin/linked_list_challenges/linked_list_challenges.dart
+++ bin/linked_list_challenges/linked_list_challenges.dart
@@ -1,9 +1,15 @@
 import 'package:data_structure_submission/linked_list.dart';
 
+import 'challenge1.dart';
+
 void main(List<String> args) {
+  //Linked List
   final list = LinkedList<int>();
   list.push(3);
   list.push(2);
   list.push(1);
   print(list);
+
+//print linked list in reverse
+  printListInReverse(list);
 }
