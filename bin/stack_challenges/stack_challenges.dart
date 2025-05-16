035fe9b7f4f4353dbecb3b946d4a2a67bbcad5fb
Asil Awad <aseelawad8@hotmail.com>
2025-05-16 21:14:53 +0300

    stack main function

---
+++ bin/stack_challenges/stack_challenges.dart
@@ -0,0 +1,13 @@
+import 'package:data_structure_submission/stack.dart';
+
+void main(List<String> args) {
+  final stack = Stack<int>();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+stack.push(4);
+print(stack);
+final element = stack.pop();
+print('Popped: $element');
+}
