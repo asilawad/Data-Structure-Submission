52d5cfa567aafe7631ddb7dd27bf158ed310c07d
Asil Awad <aseelawad8@hotmail.com>
2025-05-16 22:10:32 +0300

    test printInReverse

--- bin/linked_list_challenges/linked_list_challenges.dart
+++ bin/linked_list_challenges/linked_list_challenges.dart
@@ -8,8 +8,11 @@ void main(List<String> args) {
   list.push(3);
   list.push(2);
   list.push(1);
+
   print(list);
 
 //print linked list in reverse
+  print('Original list: $list');
+  print("Printing in reverse:");
   printListInReverse(list);
 }
